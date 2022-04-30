eb7c54fcd13177fa34e57ac272a2c523330a180a
Ashley Lampard <ashleylampard@live.com>
2022-04-30 11:17:46 +0100

    created api linked db seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,27 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+
+response = URI.open("https://tmdb.lewagon.com/movie/top_rated")
+json = JSON.parse(response.read)
+
+
+
+puts "Cleaning database..."
+Movie.destroy_all
+
+puts "Creating restaurants..."
+
+json['results'].each do |entry|
+  movie = Movie.create!(
+    title: entry['title'],
+    overview: entry['overview'],
+    rating: entry['vote_average'],
+    poster_url: "https://image.tmdb.org/t/p/original/#{entry['poster_path']}"
+  )
+
+  puts "Created #{movie.title}"
+end
+
+puts "Finished!"
