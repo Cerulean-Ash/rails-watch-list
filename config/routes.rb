e6cc5f0c7fd6b466e31507b329f301552021d598
Ashley Lampard <ashleylampard@live.com>
2022-05-04 10:49:50 +0100

    updated new_review in list controller

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,7 @@ Rails.application.routes.draw do
   root to: 'lists#index'
   resources :lists, only: [ :index, :new, :show, :create, :destroy ] do
     resources :bookmarks, only: [ :new, :create ]
-    resources :reviews, only: [ :new, :create ]
+    resources :reviews, only: :create
   end
   resources :bookmarks, only: :destroy
   resources :reviews, only: :destroy
